cece1842d08f311ab53e0c08318abadcc66f202f
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-08-15 23:48:15 -0500

    responsive intro

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 import "./intro.css";
-import Card from "react-bootstrap/Card";
-import cardBg from "../../images/introbg.jpg";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Intro() {
 	return (
-		<Card className="intro">
-			<Card.Img src={cardBg} className="introbg"/>
-			<Card.ImgOverlay>
-				<Card.Title className="name">Joshua Martinez</Card.Title>
-				<Card.Text className="intro-text">
-					Lorem ipsum dolor sit amet, dicat propriae mei ea, duo at enim dolor
-					referrentur, te posse pericula pri. Ei vix nonumes dissentias, dolor
-					legere consectetuer quo te. Lorem nostro gubergren ad quo. Et insolens
-				</Card.Text>
-			</Card.ImgOverlay>
-		</Card>
+		<Container fluid className="intro">
+			<Row>
+				<Col>
+					<h1 className="name">Joshua Martinez</h1>
+				</Col>
+			</Row>
+			<Row>
+				<Col md={{ span: 6, offset: 3 }}>
+					<p className="intro-text">
+						Lorem ipsum dolor sit amet, dicat propriae mei ea, duo at enim dolor
+						referrentur, te posse pericula pri. Ei vix nonumes dissentias, dolor
+						legere consectetuer quo te. Lorem nostro gubergren ad quo. Et
+						insolens instructior his, vis odio propriae gubergren in, cu rebum
+						erroribus consequuntur mel. Te sed nobis graeco. Ridens iriure vix
+						at. Ut pro graeco aliquip, ei sapientem temporibus vel. Possim
+						eripuit ut nec, id ponderum voluptatibus quo. Ea dicit tibique
+						periculis mea, phaedrum
+					</p>
+				</Col>
+			</Row>
+		</Container>
 	);
 }
 
