c47d51ba642f8afdff51aac7ee62df209668be36
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-08-17 18:11:09 -0500

    updated

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -5,19 +5,17 @@ import Portcard from "../Portcard";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-function Portfolio() {
+function Portfolio(props) {
+	const portCardList = props.portfolio.map((project) => (
+		<Portcard key={project.id} title={project.title} url={project.url} repo={project.repository} imageLocation={project.image} text={project.text} />
+	))
 	return (
 			<Container fluid className="portfolio">
 				<Row>
 					<Col><h2 className="text-center portfolioText">Portfolio</h2></Col>
 				</Row>
 				<Row sm={1} md={3} className="cardDiv">
-					<Portcard />
-					<Portcard />
-					<Portcard />
-					<Portcard />
-					<Portcard />
-					<Portcard />
+					{portCardList}
 				</Row>
 			</Container>
 	);
