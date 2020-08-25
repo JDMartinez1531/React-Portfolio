bfcd5262c2dca3e783319072adfdad9fca78c47c
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-08-25 12:18:39 -0500

    added gh pages branch

---
+++ build/precache-manifest.df7c99b4be774bf286ee1b96a14f4e6d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a76f15f1372526494312a68e8116e03",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "23a5f7af7f94ffaef963",
+    "url": "/React-Portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "415ff761f12fac82e791",
+    "url": "/React-Portfolio/static/css/main.b8a6c04a.chunk.css"
+  },
+  {
+    "revision": "23a5f7af7f94ffaef963",
+    "url": "/React-Portfolio/static/js/2.2a90c1aa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Portfolio/static/js/2.2a90c1aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "415ff761f12fac82e791",
+    "url": "/React-Portfolio/static/js/main.477a8286.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "10cc02254aea6ba4bcb2aa770bae090e",
+    "url": "/React-Portfolio/static/media/contactbg.10cc0225.jpg"
+  },
+  {
+    "revision": "977c58ec673517bf3c32f41bf4ba951e",
+    "url": "/React-Portfolio/static/media/demo.977c58ec.gif"
+  },
+  {
+    "revision": "ca2c29431d549295488bfff1a42f6eaa",
+    "url": "/React-Portfolio/static/media/goldcog.ca2c2943.jpg"
+  },
+  {
+    "revision": "fa6121693750b1a87d13db906bac2219",
+    "url": "/React-Portfolio/static/media/jdmresume2020.fa612169.pdf"
+  },
+  {
+    "revision": "69582e6298bf84b0c9f6857a85bfb237",
+    "url": "/React-Portfolio/static/media/lightbg.69582e62.jpg"
+  },
+  {
+    "revision": "0ee428ae97c7c97d2e631c2dbfb7fb70",
+    "url": "/React-Portfolio/static/media/password.0ee428ae.jpg"
+  },
+  {
+    "revision": "a1222aaa185b26affaad9ff51fdccc50",
+    "url": "/React-Portfolio/static/media/profile-05.a1222aaa.jpg"
+  },
+  {
+    "revision": "d5d1bf9eb537c21fedc0cd56e5464dee",
+    "url": "/React-Portfolio/static/media/quiz.d5d1bf9e.png"
+  },
+  {
+    "revision": "f583bf495419f058b9dec1abe3763f8b",
+    "url": "/React-Portfolio/static/media/teampro.f583bf49.png"
+  },
+  {
+    "revision": "aee4d32fbce4587400821b7729352fcd",
+    "url": "/React-Portfolio/static/media/travelb.aee4d32f.png"
+  },
+  {
+    "revision": "53acd7c4e72cf35cf36846f45740b1cc",
+    "url": "/React-Portfolio/static/media/weather.53acd7c4.png"
+  }
+]);
