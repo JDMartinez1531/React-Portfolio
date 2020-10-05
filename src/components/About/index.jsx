e8e77208c0b95105fcecfc44a3b67395cfd5bf60
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-10-05 14:04:18 -0500

    final update

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -40,33 +40,3 @@ function About() {
 }
 
 export default About;
-
-/* <Container fluid className="about-section">
-			<Row>
-				<Col xs={6} md={4}>
-					<Image src={profileImage} roundedCircle />
-				</Col>
-				<Col className="about-text">
-					<h1>About me</h1>
-					<p className="aboutP">
-						Lorem ipsum dolor sit amet, dicat propriae mei ea, duo at enim dolor
-						referrentur, te posse pericula pri. Ei vix nonumes dissentias, dolor
-						legere consectetuer quo te. Lorem nostro gubergren ad quo. Et
-						insolens instructior his, vis odio propriae gubergren in, cu rebum
-						erroribus consequuntur mel. Te sed nobis graeco. Ridens iriure vix
-						at. Ut pro graeco aliquip, ei sapientem temporibus vel. Possim
-						eripuit ut nec, id ponderum voluptatibus quo. Ea dicit tibique
-						periculis mea, phaedrum
-					</p>
-				</Col>
-			</Row>
-		</Container> */
-
-// <img
-// 		width={150}
-// 		height={150}
-// 		borderRadius={50}
-// 		className="profile align-self-center ml-4 mr-3"
-// 		src={profileImage}
-// 		alt="handsome guy"
-// 	/>
